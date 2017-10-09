136bfc0915b32bd9cefb8698936823ec570ad780
ankon <ankon@ankon-mac.local>
2017-10-09 20:12:49 +0800

    when created a group, join it automatically

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -14,10 +14,11 @@ class GroupsController < ApplicationController
     @group.user = current_user
 
     if @group.save
-
+      current_user.join!(@group)
       redirect_to groups_path
-    else render :new
-  end
+    else
+      render :new
+    end
   end
 
   def show
